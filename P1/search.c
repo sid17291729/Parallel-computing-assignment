2adec3784ed114159bb1580a1f9962e24e5a007e
sid17291729 <siddharth.m.awasthi7@gmail.com>
2021-03-22 18:06:58 +0530

    changes to search.c

--- P1/search.c
+++ P1/search.c
@@ -10,29 +10,31 @@
 //echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
 
 int eof;
+
+/*** Utility functions***/
 int get_file_size(int fd)
 {
     struct stat buf;
     fstat(fd,&buf);
-    //printf("fsize=%d\n",(int)buf.st_size);
+    
     return buf.st_size;
 }
 void set_pointer(int fd,int offset)
 {
     lseek(fd,offset,SEEK_SET);
 }
+//correcting the pointer to point to the begining of the word initially.This is done because the region for a process might start in the middle of a particular word.
 void correct(int fd,int *line,int *count,int rank)
 {
     lseek(fd,-1,SEEK_CUR);
     char c;
     read(fd,&c,1);
     
-    //printf("rank=%d ,c=%c\n",rank,c);
+    
     if(c==32||c==10||c==13)
     {   
         while(c==' '||c=='\n'||c==13)
-       {   //if(c==10)
-            //(*line)=(*line)+1;
+       {   
             
            int x=read(fd,&c,1);
            if(x==0)
@@ -43,7 +45,7 @@ void correct(int fd,int *line,int *count,int rank)
         }
         lseek(fd,-1,SEEK_CUR);
         (*count)=(*count)-1;
-        //printf("c=%c\n",c);
+        
         return;
     }
     int x=read(fd,&c,1);
@@ -58,9 +60,10 @@ void correct(int fd,int *line,int *count,int rank)
         if(x==-1)
         return;
     }
-    // if(c=='\n'||c==13)
-    // (*line)=(*line)+1;
+    
 }
+
+//Linked list Node
 typedef struct node{
     char *word;
     int win;
@@ -68,7 +71,7 @@ typedef struct node{
     int wlen;
     struct node *next;
 }NODE;
-
+// reading one word at a time from the file
 int get_word(int fd,char *buf,int *wlen,int *line)
 {
     char c;
@@ -82,7 +85,7 @@ int get_word(int fd,char *buf,int *wlen,int *line)
      while(c!=' '&&c!='\n')
      {
        x=read(fd,&c,1);
-      //printf("c=%c\n",c);
+      
        if(x==0)
        { eof=1;
         return -1*count;}
@@ -109,8 +112,7 @@ int get_word(int fd,char *buf,int *wlen,int *line)
      lseek(fd,-1,SEEK_CUR);
      return count;
 }
-
-
+//This data structure is used to store the positions of the query word found in the file
 typedef struct position
 {
  int wno;
@@ -119,6 +121,7 @@ typedef struct position
  int *ln;
  int *win;   
 }POS;
+
 void pos_init(POS * p,int wno)
 {
  p->cap=10;
@@ -162,45 +165,51 @@ int check(NODE *head,char *q_word[],int Q_size)
     return 1;
     
 }
-
+/***main function***/
 int main(int argc, char*argv[] )
 {
     int rank,num_tasks;
     MPI_Status Stat;
+    //setting up processes
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&num_tasks);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    
+    //opening file and adjusting the pointers for each process
     int fd=open(argv[1],O_RDONLY);
     int file_size=get_file_size(fd);
-    //printf("file size=%d\n",file_size);
     int local_size=file_size/num_tasks;
     set_pointer(fd,local_size*rank);
+    
+    //initializing
     int line=0;
     int count=0;
     if(rank!=0)
     correct(fd,&line,&count,rank);
     if(rank!=num_tasks-1)
     eof=1;
-    //printf("file size=%d, local suze=%d\n",file_size,local_size);
-    //printf("count=%d\n",count);
     int word_index=1;
-    
     int AND=0;
+    
+    //checking whether it is an AND query or a OR query
     if(strcmp(argv[2],"OR")==0)
     AND=0;
     else
     AND=1;
-    int Q_size=argc-3;
+
+    int Q_size=argc-3;//size of the query
     int qsz;
     if(AND)
     qsz=1;
     else
     qsz=Q_size;
-    POS q[qsz];
-    char *q_word[Q_size];
-    //printf("rank=%d,Qsize=%d\n",rank,Q_size);
+
+    POS q[qsz];//vector to hold the results
+    char *q_word[Q_size];//vector to hold the query
     if(AND)
     pos_init(&q[0],0);
+    
+    //populating the query vector
     for(int i=0;i<Q_size;++i)
     {  if(!AND)
         pos_init(&q[i],i);
@@ -208,67 +217,60 @@ int main(int argc, char*argv[] )
       
      strcpy(q_word[i],argv[i+3]);
     }
-    //printf("reached here\n");
-    char buf[MAXLEN];
+    
+    char buf[MAXLEN];//temporary buffer
     double time_start,time_end,time_diff,final_time;
     MPI_Barrier(MPI_COMM_WORLD);
     time_start=MPI_Wtime();
 
-    switch (AND)
+    switch (AND)//different cases will be executed based on the type of query
     {   case 0:{
-        int flag=1;
-        while((count<local_size)||!eof)
+        //OR case
+        while((count<local_size)||!eof)//this loop runs untill either we have reached the EOF or read the process's region completely
         {   
             buf[0]='\0';
             int wlen=0;
-          
+            //storing the line number and the wordindex on the line of the current word          
             int w_line=line;
             int w_in=word_index;
+            
             int end=get_word(fd,buf,&wlen,&line);
             buf[wlen]='\0';
-            // if(flag)
-            // {
-            //     printf("Rank=%d,first word buf=%s\n",rank,buf);
-            //     flag=0;
-            // }
+            
+            //updating the line number and word index for the next word that will be read in the next iteration
             if(w_line!=line)
             word_index=1;
             else
             ++word_index;
+            //checking whether the word matches with any of the query word
             for(int i=0;i<Q_size;++i)
             {
                 if(strcmp(q_word[i],buf)==0)
                 {
                  add_word(&q[i],w_in,w_line,i);
-                 //printf("rank=%d, word=%s ,win=%d, ln=%d \n",rank,q_word[i],q[i].win[q[i].ind-1],q[i].ln[q[i].ind-1]);
+                 
                 }
             }
              if(end<0)
             {
             count=count+-1*end;
-            //printf("rank= %d, wlen=%d \n",rank,wlen);
-            
-            //printf("rank= %d, first_word=%s line=%d \n",rank,buf,line);
+           
             continue;
-            //break;
+            
             }
             count+=end;
-         //printf("rank= %d, count=%d \n",rank,count);
-         //buf[wlen]='\0';
-         //printf("rank= %d, first_word=%s  end=%d \n",rank,buf,end);
-            //printf("rank=%d, windex=%d\n",rank,word_index);
-            
+         
     
         }
-        //printf("rank=%d, last word buf=%s,count=%d\n",rank,buf,count);
+        //OR case finished
         break;
         }
-        case 1:
+        case 1://AND case
         {  NODE *head;
            NODE *tail; 
            head=(NODE *)malloc(sizeof(NODE));
            NODE *t=head;
-            for(int i=0;i<Q_size;++i)
+            for(int i=0;i<Q_size;++i)//storing the first Q_size no. of words in a linked list 
             {
               buf[0]='\0';
               int wlen=0;
@@ -305,10 +307,10 @@ int main(int argc, char*argv[] )
             while((count<local_size)||!eof)
             {
                 if(check(head,q_word,Q_size))
-                {add_word(&q[0],head->win,head->lno,0);
+                {add_word(&q[0],head->win,head->lno,0);//comparing the contents of the linked list with the query
                 }
                 
-
+              // reading the word and updating the linked list
                 buf[0]='\0';
                 int wlen=0;
                 int w_line=line;
@@ -343,6 +345,7 @@ int main(int argc, char*argv[] )
              int extra=1;
              int temp_line=line;
              int temp_w_ind=word_index;
+             //we will have to check the case where some words lie in the region of another process
              while(extra<=Q_size)
              {
                  if(check(head,q_word,Q_size))
@@ -382,11 +385,7 @@ int main(int argc, char*argv[] )
                  }
              }
                 
-             //pl(head,rank);
-            //  for(int i=0;i<q[0].ind;++i)
-            //  printf("rank=%d, wno=%d, lno=%d\n",rank,q[0].win[i],q[0].ln[i]);
-            //  MPI_Finalize();
-            // return 0;
+             
           break;
         }
 
@@ -395,16 +394,11 @@ int main(int argc, char*argv[] )
     time_diff=time_end-time_start;
     
     MPI_Reduce(&time_diff,&final_time,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
-    // if(rank==0)
-    // {
-    //     for(int i=0;i<num_tasks;++i)
-    //     {
-    //         MPI_SEND
-    //     }
-    // }
+    
 
     int w_recv,line_recv;
-    //MPI_Scan(&word_index,&w_recv,1,MPI_INT,MPI_SUM,MPI_COMM_WORLD);
+    /* getting the no. of lines read till the previous process so that we can get the global index of the word in the file.
+    Till now we only knew the index relative to the region assigned to the process*/
     if(num_tasks>1)
     MPI_Scan(&line,&line_recv,1,MPI_INT,MPI_SUM,MPI_COMM_WORLD);
     if(rank==0&&num_tasks>1)
@@ -420,14 +414,14 @@ int main(int argc, char*argv[] )
         }
     }
     if(rank!=0)
-    {//w_recv=w_recv-word_index;
+    {
      line_recv=line_recv-line;
     }
-    //printf("yes\n");
-    //printf("rank =%d ,w_recv=%d, line_recv=%d\n",rank,w_recv,line_recv);
+    
     int pword,pline;
     int *lno_send;
     int *win_send;
+    //calculating and sending the results to process 0
     if(rank!=0)
     {   
         for(int i=0;i<qsz;++i)
@@ -444,16 +438,16 @@ int main(int argc, char*argv[] )
                 {   pword=q[i].win[j];
                     pline+=q[i].ln[j];
                 }
-                 //printf("rank =%d ,wno=%d, word_index=%d, line_no=%d\n",rank,q[i].wno,pword,pline);
+                 
                 lno_send[j]=pline;
                 win_send[j]=pword;
 
             }
-            //printf("rank=%d, size=%d\n",rank,q[i].ind);
+            \
             MPI_Send(&q[i].ind,1,MPI_INT,0,0,MPI_COMM_WORLD);
             MPI_Send(lno_send,q[i].ind,MPI_INT,0,0,MPI_COMM_WORLD);
             MPI_Send(win_send,q[i].ind,MPI_INT,0,0,MPI_COMM_WORLD);
-            //printf("rank=%d sent= %d\n",rank,s);
+           
             free(lno_send);
             free(win_send);
     
@@ -462,7 +456,7 @@ int main(int argc, char*argv[] )
         
         
     }
-    else
+    else//process 0 recieving and printing all the results
     {   for(int i=0;i<qsz;++i)
        {     lno_send=(int *)malloc(sizeof(int)*100);
              win_send=(int *)malloc(sizeof(int)*100);
@@ -484,8 +478,7 @@ int main(int argc, char*argv[] )
              MPI_Recv(&sz,1,MPI_INT,j,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
              MPI_Recv(lno_send,100,MPI_INT,j,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
              MPI_Recv(win_send,100,MPI_INT,j,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-            // printf("rank=%d recieved= %d\n",rank,rc);
-            //printf("recieved from rank=%d\n",j);
+            
              for(int k=0;k<sz;++k)
              {
                  printf("line no.=%d word index=%d\n",lno_send[k],win_send[k]);
@@ -496,12 +489,9 @@ int main(int argc, char*argv[] )
              free(win_send);
        }
     }
-    // if(w1.ind!=0)
-    //     printf("rank =%d ,word_index=%d, line_no=%d\n",rank,pword,pline);
-
-    //printf("rank =%d ,wrcv=%d, lircv=%d\n",rank,w_recv,line_recv);
-    if(rank==0)
-    printf("Max time=%f\n",final_time);
+    
+    // if(rank==0)
+    // printf("Max time=%f\n",final_time);
     MPI_Finalize();
 
 }
